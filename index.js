80d6dd521db8452a2003edfd8762ae44332878ce
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-16 14:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const expect = require('expect')
-const fs = require(fs)
-const jsdom = require('jsdom')
-const path = require('path')
-
-// describe('index', () => {
-//
-// })
-//
-// it('cals console.error()', () => {
-//
-// })
-//
-// jsdom({
-//   src: fs.readFileSync(path.resolve(__dirname, '..','index.js'),'utf-8')
-// })
+console.error("HALP!")
